460ce68b231fb366e1739217c65ddf5a2e89ce87
zacwillmington <zac.willmington@gmail.com>
2019-05-13 19:27:57 -0700

    Created components for pages of site.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
+import DUISPage from './components/DUISPage';
+import CriminalDefencePage from './components/CriminalDefencePage';
+import PersonalInjuryPage from './components/PersonalInjuryPage';
+import Hompage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
       </header>
+      <React.Fragment>
+        <HomePage />
+        <AboutPage />
+        <ContactPage />
+        <DUISPage />
+        <PersonalInjuryPage />
+        <CriminalDefencePage />
+      </React.Fragment>
     </div>
   );
 }
