04c7038966e669808403382e8c5a8d8fb9c16579
zacwillmington <zac.willmington@gmail.com>
2019-05-20 13:39:16 -0500

    Added content to HomePage component.

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div>
-              <h1>HomePage</h1>  
+            <div className='homePage'>
+              <div className='content-1'><h4><span>Severcing all of San Diego and Riverside counties</span></h4></div>  
+
+              <div className='content-2'><h3><span>New clients will receive 20% off of their case. Case evaluations are free!</span></h3></div>  
             </div>
         );
     }
