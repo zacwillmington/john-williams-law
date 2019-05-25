51cdc5ed8db360388cc2595c4ecc8b4a2204fb2b
zacwillmington <zac.willmington@gmail.com>
2019-05-25 12:30:06 -0500

    Changing layout for home page.

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -21,18 +21,18 @@ class HomePage extends Component {
                     <p>Aggressive, ethical representation purposed to achieve the most favorable outcome recognizing the facts of each case, the applicable law, and the unique circumstances that each client brings.</p>
                 </div>
                 <div className='bottom-content'>
-                    <div><h1>PRACTICE AREAS</h1></div>
+                    <div className='title-bottom-content'><h1>PRACTICE AREAS</h1></div>
                     <div className='law-book'>
                         <img id='law-book' src='https://res.cloudinary.com/dmjs1tfzn/image/upload/v1558735806/Screen_Shot_2019-05-23_at_4.18.37_PM.png' alt='law-book'></img>
-                        <span>PERSONAL INJURY</span>
+                        <span id='img-span-law-book'>PERSONAL INJURY</span>
                     </div>
                     <div className='jail-cell'>
                         <img id='jail-cell' src='https://res.cloudinary.com/dmjs1tfzn/image/upload/v1558735804/Screen_Shot_2019-05-23_at_4.19.23_PM.png' alt='jail-cell'></img>
-                        <span>CRIMINAL DEFENCE</span>    
+                        <span id='img-span-jail-cell'>CRIMINAL DEFENCE</span>    
                     </div>
                     <div className='breathlyser'>
                         <img id='breathalyser' src='https://res.cloudinary.com/dmjs1tfzn/image/upload/v1558735801/Screen_Shot_2019-05-23_at_4.20.19_PM.png' alt='breathalyser'></img>
-                        <span>DUI'S</span>
+                        <span id='img-span-breathlyser'>DUI'S</span>
                     </div>
                 </div>
         </div>
