9adc9db99bd10849ad49d7345476c93ad0834134
zacwillmington <zac.willmington@gmail.com>
2019-05-26 10:15:29 -0500

    Styled personal injury page.

--- client/src/components/PersonalInjuryPage.js
+++ client/src/components/PersonalInjuryPage.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class PersonalInjuryPage extends Component {
     render() {
         return (
-            <div>
-                <h1>An Attorney Who Will Fight For The Settlement You Deserve</h1>
+            <div className='personal-injury-page'>
+                <h2>An Attorney Who Will Fight For The Settlement You Deserve</h2>
                 <p>From soft tissue to catastrophic injury including wrongful death, John Williams has over three decades of experience presenting and pursuing claims on behalf of his clients. Automobile accident, motorcycle accident, premises liability (slip and fall), dog bite, medical malpractice, uninsured/under insured claims, John Williams has demonstrated success in these arenas. For a no obligation, free consultation of your potential personal injury/wrongful death case call John Williams.</p>
             </div>
         );
