af77b6e4e1e7a325b7135ca611027b974f1ee468
zacwillmington <zac.willmington@gmail.com>
2019-05-31 17:23:25 -0500

    Changed logo to png for heroku hosting

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,13 @@ import { Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
-
+//logo https://static.wixstatic.com/media/5d49f2_bff266cfa6b548a883a98712f38a248a~mv2.jpg/v1/fill/w_88,h_90,al_c,q_80,usm_0.66_1.00_0.01/5d49f2_bff266cfa6b548a883a98712f38a248a~mv2.webp
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img id='logo' src='https://static.wixstatic.com/media/5d49f2_bff266cfa6b548a883a98712f38a248a~mv2.jpg/v1/fill/w_88,h_90,al_c,q_80,usm_0.66_1.00_0.01/5d49f2_bff266cfa6b548a883a98712f38a248a~mv2.webp'></img>
+      <img id='logo' src='https://res.cloudinary.com/dmjs1tfzn/image/upload/v1559341191/Screen_Shot_2019-05-31_at_5.19.21_PM.png'></img>
         <NavBar />
       </header>
       <React.Fragment>
